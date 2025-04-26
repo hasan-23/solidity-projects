4260cd09f15d57387ed048587357e34a3fc67a27
Md. Hasan Tanvir <hasan.web3.eng@gmail.com>
2025-04-26 11:33:39 +0600

    Update AccessVault.sol with comments and polish

--- contracts/AccessVault.sol
+++ contracts/AccessVault.sol
@@ -4,25 +4,30 @@ pragma solidity ^0.8.0;
 contract AccessVault {
     address public owner;
 
+    // Sets the deployer as the contract owner
     constructor() {
         owner = msg.sender;
     }
 
+    // Modifier to restrict access to only the owner
     modifier onlyOwner() {
         require(msg.sender == owner, "Access denied: not the owner");
         _;
     }
 
+    // Event emitted when ETH is deposited
     event Deposited(address indexed sender, uint amount);
+
+    // Event emitted when ETH is withdrawn
     event Withdrawn(address indexed receiver, uint amount);
 
-    // Deposit ETH into the contract
+    // Allows the owner to deposit ETH into the contract
     function deposit() public payable onlyOwner {
         require(msg.value > 0, "Must send ETH to deposit");
         emit Deposited(msg.sender, msg.value);
     }
 
-    // Withdraw ETH from the contract
+    // Allows the owner to withdraw a specified amount of ETH
     function withdraw(uint _amount) public onlyOwner {
         require(_amount > 0, "Withdraw amount must be greater than 0");
         require(address(this).balance >= _amount, "Insufficient contract balance");
@@ -31,7 +36,7 @@ contract AccessVault {
         emit Withdrawn(msg.sender, _amount);
     }
 
-    // View current contract balance
+    // Returns the current balance of the contract
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
